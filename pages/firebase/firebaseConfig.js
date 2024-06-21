d3988a77a5ce0fbf6ca96c25a6585747da1276e9
stayhomewelldone <0973013@hr.nl>
2024-06-21 16:46:04 +0200

    feat: Implemented functionality to send hobbies, interests and user information to database. Also added functionality to distinguish hobbies and interests that are already added.

--- pages/firebase/firebaseConfig.js
+++ pages/firebase/firebaseConfig.js
@@ -1,13 +1,19 @@
 import { initializeApp } from 'firebase/app';
 import { getStorage } from 'firebase/storage';
+import { getFirestore } from "firebase/firestore";
+
 
 
 const firebaseConfig = {
+    projectId: "ace-booth",
+
     storageBucket: "gs://ace-booth.appspot.com",
+    
 
 }
 
 const app = initializeApp(firebaseConfig);
 const storage = getStorage(app);
+const db = getFirestore(app);
 
-export { storage };
+export { storage, db };
