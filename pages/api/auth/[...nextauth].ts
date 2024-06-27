3c41d685accd522edf18f27db57f20010317706f
stayhomewelldone <0973013@hr.nl>
2024-06-27 20:55:00 +0200

    Fix some authentication issues with NextAuth.js

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -7,7 +7,7 @@ export const authOptions  = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET
     })
   ],
-  secret: process.env.SECRET,
+  secret: process.env.NEXTAUTH_SECRET,
 }
 
 export default NextAuth(authOptions)
